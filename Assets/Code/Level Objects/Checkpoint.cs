2554c9d8cd4f8959991d3cf358161b13b1b3840b
AngrySushiroll8 <MTMassey@student.fullsail.edu>
2025-09-09 13:50:14 -0400

    Hopefully have the Checkpoint system working.

--- Assets/Code/Level Objects/Checkpoint.cs
+++ Assets/Code/Level Objects/Checkpoint.cs
@@ -1,14 +1,14 @@
-//using UnityEngine;
-//using System.Collections;
+using UnityEngine;
+using System.Collections;
 
-//public class Checkpoint : MonoBehaviour
-//{
-//    [SerializeField] Renderer player;
-//    private void OnTriggerEnter(Collider other)
-//    {
-//        if(other.CompareTag("Player") && GameManager.instance.playerSpawn.transform.position != transform.position)
-//        {
-//            GameManager.instance.playerSpawn.transform.position = transform.position;
-//        }
-//    }
-//}
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField] Renderer player;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && GameManager.instance.playerSpawnPos.transform.position != transform.position)
+        {
+            GameManager.instance.playerSpawnPos.transform.position = transform.position;
+        }
+    }
+}
