d7f0fb922c1139319f5cea5473f6b9534c27c594
HunterHuelsmann <hunter.huelsmann07@gmail.com>
2025-09-10 15:11:06 -0400

    Pistol, Shotgun, And Bloom

--- Assets/Code/Player/PlayerShoot.cs
+++ Assets/Code/Player/PlayerShoot.cs
@@ -1,5 +1,4 @@
-using System.Threading;
-using Unity.VisualScripting;
+using System.Collections.Generic;
 using UnityEngine;
 
 class PlayerShoot : MonoBehaviour
@@ -9,13 +8,37 @@ class PlayerShoot : MonoBehaviour
     [SerializeField] float fireDistance;
     [SerializeField] int bullets;
     [SerializeField] float bulletSpeed;
+    [SerializeField] float bloomMod;
     [SerializeField] int ammo;
     [SerializeField] bool isAutomatic;
     [SerializeField] GameObject shootPoint;
     [SerializeField] GameObject bulletPrefab;
     [SerializeField] LayerMask shootMask;
+    [SerializeField] List<GunStats> gunList;
 
     float fireTimer = 0;
+
+    void Start()
+    {
+        gunList[0].ammo = gunList[0].maxAmmo;
+        SwitchWeapons(gunList[0]);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Weapon1"))
+        {
+            SwitchWeapons(gunList[0]);
+        }
+        else if (gunList.Count > 1 && Input.GetButtonDown("Weapon2"))
+        {
+            SwitchWeapons(gunList[1]);
+        }
+        else
+        {
+            // Melee weapon
+        }
+    }
 
     public void Shoot()
     {
@@ -25,7 +48,15 @@ class PlayerShoot : MonoBehaviour
             for (int bullet = 0; bullet < bullets; bullet++)
             {
                 RaycastHit hit;
-                if (Physics.Raycast(shootPoint.transform.position, shootPoint.transform.forward, out hit, fireDistance, shootMask))
+                float rangeX = Random.Range(-bloomMod, bloomMod);
+                float rangeY = Random.Range(-bloomMod, bloomMod);
+                if (Physics.Raycast(shootPoint.transform.position,
+                                    new Vector3(Camera.main.transform.forward.x + rangeX,
+                                                Camera.main.transform.forward.y + rangeY,
+                                                Camera.main.transform.forward.z),
+                                    out hit,
+                                    fireDistance,
+                                    shootMask))
                 {
                     IDamage dmg = hit.transform.GetComponent<IDamage>();
                     if (dmg != null)
@@ -33,7 +64,7 @@ class PlayerShoot : MonoBehaviour
                         dmg.TakeDamage(damage);
                     }
                 }
-                Bullet bulletObj = Instantiate(bulletPrefab, shootPoint.transform.position, Camera.main.transform.rotation, null).GetComponent<Bullet>();
+                Bullet bulletObj = Instantiate(bulletPrefab, shootPoint.transform.position, new Quaternion(Camera.main.transform.rotation.x + rangeX, Camera.main.transform.rotation.y + rangeY, Camera.main.transform.rotation.z, Camera.main.transform.rotation.w), null).GetComponent<Bullet>();
                 bulletObj.targetPoint = hit.point;
                 bulletObj.distance = fireDistance;
                 bulletObj.bulletSpeed = bulletSpeed;// * (hit.transform != null ? Vector3.Distance(shootPoint.transform.position, hit.point) : );
@@ -41,5 +72,16 @@ class PlayerShoot : MonoBehaviour
             ammo--;
             fireTimer = 0;
         }
+    }
+
+    void SwitchWeapons(GunStats stats)
+    {
+        damage = stats.damage;
+        fireRate = stats.fireRate;
+        fireDistance = stats.fireDistance;
+        bloomMod = stats.bloomMod;
+        bullets = stats.bullets;
+        ammo = stats.ammo;
+        isAutomatic = stats.isAutomatic;
     }
 }
