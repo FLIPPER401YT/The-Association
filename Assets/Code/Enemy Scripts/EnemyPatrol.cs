d4bb5f91e62a84b008cc800fe2af3cd8e27b361f
AngrySushiroll8 <MTMassey@student.fullsail.edu>
2025-09-11 14:07:15 -0400

    Beginning base framework for enemy patrols

---
+++ Assets/Code/Enemy Scripts/EnemyPatrol.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [SerializeField] GameObject patrolPoint1, patrolPoint2, patrolPoint3, patrolPoint4;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
