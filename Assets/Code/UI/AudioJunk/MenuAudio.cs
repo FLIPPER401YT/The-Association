86884dab0cbbc1e0315758cb8cb205bd62d60847
Dominic Thompson <DWThompson2@student.fullsail.edu>
2025-09-27 09:09:25 -0400

    Main menu music is here YIPPEE

--- Assets/Code/UI/AudioJunk/MenuAudio.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "MenuSounds", menuName = "Scriptable Objects/MenuSounds")]
-
-public class MenuAudio : ScriptableObject
-{
-    public AudioClip previewSound;
-}
